d1e003c461632834b88a59a5d12d7a7330b4ace1
Xiaobo <18580902653@163.com>
2024-02-01 22:47:22 +0800

    update qna3

--- web3go/HW_Web3GoCheckIn.py
+++ web3go/HW_Web3GoCheckIn.py
@@ -52,8 +52,8 @@ class Task(QLTask):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
         }
-        session = get_chrome_session()
-        # session = requests.Session()
+        # session = get_chrome_session()
+        session = requests.Session()
         session.headers.update(headers)
         session.proxies = proxy
         token = login(session, address, private_key)

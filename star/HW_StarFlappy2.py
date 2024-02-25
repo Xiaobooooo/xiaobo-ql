120a22921fff7152c3aa744726fbf46f7e908ea8
Xiaobo <18580902653@163.com>
2024-02-25 16:47:50 +0800

    update star

--- star/HW_StarFlappy2.py
+++ star/HW_StarFlappy2.py
@@ -4,7 +4,7 @@ new Env('Star_Flappy_小号')
 """
 import requests
 
-from common.task import QLTask
+from common.task import QLTask, get_proxy
 from common.util import log
 from HW_StarLogin import get_headers
 from HW_StarFlappy import game_record
@@ -28,11 +28,16 @@ class Task(QLTask):
         session.headers.update(get_headers(token))
         session.proxies = {'https': proxy}
 
+        i = 0
         while True:
             result = game_record(session, self.game, 0)
             log.info(f'【{index}】{result}')
             if result.count('失败'):
                 break
+            i += 1
+            if i > 5:
+                proxy = get_proxy(self.api_url, index)
+                session.proxies = {'https': proxy}
 
 
 if __name__ == '__main__':
